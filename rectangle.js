1497885ca0f6d78adc685ccd9becfa7fc5b0901b
Ajmal Jalal <ajmaljalal58@gmail.com>
2021-12-12 20:55:55 -0800

    ES6 modules

--- rectangle.js
+++ rectangle.js
@@ -1,7 +1,7 @@
 // exports.perimeter = (x, y) => 2 * (x+y)
 // exports.area = (x, y) => x * y
 
-module.exports = (x, y, callback) => {
+export default (x, y, callback) => {
     if (x <= 0 || y <= 0) {
         callback(new Error(`Rectangle dimensions must be greater than zero. Received: ${x}, ${y}`));
     } else {
